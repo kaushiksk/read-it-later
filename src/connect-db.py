a18d268e1a0d8696f698cd7f8102aa4f34fd10ea
kaushiksk <kaushikskalmady@gmail.com>
2017-09-21 19:22:45 +0530

    Intial File structure with template code

---
+++ src/connect-db.py
@@ -0,0 +1,21 @@
+import MySQLdb as db
+
+HOST = "174.138.56.33"
+PORT = 3306
+USER = "kausid"
+PASSWORD = "kausid"
+DB = "project"
+
+try:
+        connection = db.connect(host=HOST, port=PORT, user=USER,
+                                  passwd=PASSWORD, db=DB)
+        dbhandler = connection.cursor()
+        dbhandler.execute("show tables")
+        result = dbhandler.fetchall()
+        for item in result:
+            print item
+        connection.close()
+
+except Exception as e:
+        print e
+        connection.close()

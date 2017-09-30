fb5901c13416a33982717c6abb1d49750c949162
siddharthvdn <siddharthvdn@gmail.com>
2017-09-30 23:31:59 +0530

    Use templating Make changes to the form

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, json, send_from_directory, jsonify, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 import random
-from flaskext.mysql import MySQL
 import sys
-
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 mysql = MySQL()
- 
+
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
@@ -20,25 +21,25 @@ dbhandler = connection.cursor()
 @app.route('/')
 def home():
         return render_template('index.html')
-        
+
 @app.route('/showHome')
 def showHome():
         return render_template('index.html')
-        
+
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
- 
+
     # read the posted values from the UI
     _firstName = request.form['inputFirstName']
     _lastName = request.form['inputLastName']
     _username = request.form['username']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
-    
+
     try:
     #print "INSERT INTO user(first_name, last_name, username, password, email, roll_no) VALUES('%s','%s','%s','%s','%s','%s')"%(_firstName, _lastName, _username, _password, _email, str(random.randint(1,100)))
 	    dbhandler.execute("INSERT INTO user(first_name, last_name, username, password, email, roll_no) VALUES('%s','%s','%s','%s','%s','%s')"%(_firstName, _lastName, _username, _password, _email, str(random.randint(1,100))))
@@ -53,12 +54,29 @@ def signUp():
 		data ={'msg': 'Error! Please check that all fields are valid.','type':'error'}
 		print sys.exc_info()[0]
 		return jsonify(data)
-    
+
     #return render_template('index.html')
-    
-    
-    
-    
+
+class RegisterForm(Form):
+        firstName = StringField('First name', [validators.Length(min=1, max=50)])
+        lastName = StringField('Last name', [validators.Length(min=1, max=50)])
+        username = StringField('Username', [validators.Length(min=4, max=25)])
+        email = StringField('Email', [validators.Length(min=6, max=50)])
+        rollNo = StringField('Email', [validators.Length(min=7, max=7)])
+        password = PasswordField('Password', [
+            validators.DataRequired(),
+            validators.EqualsTo('confirm', message='Passwords do not match')
+            ])
+        confirm = PasswordField
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if  request.method == 'POST' and form.validate():
+        return render_template('register.html')
+
+    return render_template('register.html', form=form)
+
 if __name__ =="__main__":
 	dbhandler.execute("describe user")
 	result = dbhandler.fetchall()
@@ -69,4 +87,3 @@ if __name__ =="__main__":
 
 	connection.commit()
 	connection.close()
-    

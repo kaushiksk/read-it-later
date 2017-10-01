72977700f3d1a9071cade75dd300127e90fded0e
siddharthvdn <siddharthvdn@gmail.com>
2017-10-01 13:53:35 +0530

    Implement Registration Resolve all bugs in previous commit

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 import random
 import sys
+# from flaskext.mysql import MySQL
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
-mysql = MySQL()
+#mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'project'
-app.config['MYSQL_DATABASE_HOST'] = '138.197.183.138'
-mysql.init_app(app)
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+# app.config['MYSQL_DATABASE_DB'] = 'project'
+# app.config['MYSQL_DATABASE_HOST'] = '138.197.183.138'
+# mysql.init_app(app)
+app.config['MYSQL_HOST'] = '138.197.183.138'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'project'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#init MYSQL_DB
+mysql = MySQL(app)
+
 #connection = db.connect(host=HOST, port=PORT, user=USER,  passwd=PASSWORD, db=DB)
-connection = mysql.connect()
-dbhandler = connection.cursor()
+#connection = mysql.connect()
+#dbhandler = connection.cursor()
 
 @app.route('/')
 def home():
         return render_template('index.html')
-
-@app.route('/showHome')
-def showHome():
-        return render_template('index.html')
-
-@app.route('/showSignUp')
-def showSignUp():
-    return render_template('signup.html')
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
@@ -58,32 +60,44 @@ def signUp():
     #return render_template('index.html')
 
 class RegisterForm(Form):
-        firstName = StringField('First name', [validators.Length(min=1, max=50)])
-        lastName = StringField('Last name', [validators.Length(min=1, max=50)])
+        firstName = StringField('First Name', [validators.Length(min=1, max=50)])
+        lastName = StringField('Last Name', [validators.Length(min=1, max=50)])
+        rollNo = StringField('Roll Number', [validators.Length(7, message='Field must be 7 characters long')])
         username = StringField('Username', [validators.Length(min=4, max=25)])
         email = StringField('Email', [validators.Length(min=6, max=50)])
-        rollNo = StringField('Email', [validators.Length(min=7, max=7)])
         password = PasswordField('Password', [
             validators.DataRequired(),
-            validators.EqualsTo('confirm', message='Passwords do not match')
+            validators.EqualTo('confirm', message='Passwords do not match')
             ])
-        confirm = PasswordField
+        confirm = PasswordField('Confirm Password')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if  request.method == 'POST' and form.validate():
-        return render_template('register.html')
+        firstName = form.firstName.data
+        lastName = form.lastName.data
+        rollNo = form.rollNo.data
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO user(first_name, last_name, username, password, email, roll_no) VALUES('%s','%s','%s','%s','%s','%s')"%(firstName, lastName, username, password, email, rollNo))
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        redirect('/')
 
     return render_template('register.html', form=form)
 
 if __name__ =="__main__":
-	dbhandler.execute("describe user")
-	result = dbhandler.fetchall()
-	for item in result:
-	    print item
-	app.config['TEMPLATES_AUTO_RELOAD'] = True
-	app.run(debug=True)
-
-	connection.commit()
-	connection.close()
+    app.secret_key = 'secret123'
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)

5c3812f217331780a736ba9b75f09b21daf00349
kaushiksk <kaushikskalmady@gmail.com>
2017-10-03 16:56:01 +0530

    Changes to front end
    Bootstrap loaded locally
    SignUp Frontend changed
    Fixed unresponsive navbar

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 import random
 import sys
-# from flaskext.mysql import MySQL
 from flask_mysqldb import MySQL
 from forms import RegisterForm
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
-#mysql = MySQL()
 
-# MySQL configurations
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-# app.config['MYSQL_DATABASE_DB'] = 'project'
-# app.config['MYSQL_DATABASE_HOST'] = '138.197.183.138'
-# mysql.init_app(app)
 app.config['MYSQL_HOST'] = '138.197.183.138'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
@@ -24,42 +16,12 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #init MYSQL_DB
 mysql = MySQL(app)
 
-#connection = db.connect(host=HOST, port=PORT, user=USER,  passwd=PASSWORD, db=DB)
-#connection = mysql.connect()
-#dbhandler = connection.cursor()
 
 @app.route('/')
 def home():
         return render_template('index.html')
 
-@app.route('/signUp',methods=['POST'])
-def signUp():
-
-    # read the posted values from the UI
-    _firstName = request.form['inputFirstName']
-    _lastName = request.form['inputLastName']
-    _username = request.form['username']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
-
-    try:
-    #print "INSERT INTO user(first_name, last_name, username, password, email, roll_no) VALUES('%s','%s','%s','%s','%s','%s')"%(_firstName, _lastName, _username, _password, _email, str(random.randint(1,100)))
-	    dbhandler.execute("INSERT INTO users(first_name, last_name, username, password, email, roll_no) VALUES('%s','%s','%s','%s','%s','%s')"%(_firstName, _lastName, _username, _password, _email, str(random.randint(1,100))))
-	    dbhandler.execute("select * from users")
-	    result = dbhandler.fetchall()
-	    for item in result:
-	    	print item
-	    data = {'msg':'New User Registered!','type':'msg'}
-	    return jsonify(data)
-
-    except:
-		data ={'msg': 'Error! Please check that all fields are valid.','type':'error'}
-		print sys.exc_info()[0]
-		return jsonify(data)
-
-    #return render_template('index.html')
-
-
+# User signup
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
---
+++ src/legacy.py
@@ -0,0 +1,26 @@
+@app.route('/signUp',methods=['POST'])
+def signUp():
+
+    # read the posted values from the UI
+    _firstName = request.form['inputFirstName']
+    _lastName = request.form['inputLastName']
+    _username = request.form['username']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    try:
+    #print "INSERT INTO user(first_name, last_name, username, password, email, roll_no) VALUES('%s','%s','%s','%s','%s','%s')"%(_firstName, _lastName, _username, _password, _email, str(random.randint(1,100)))
+	    dbhandler.execute("INSERT INTO users(first_name, last_name, username, password, email, roll_no) VALUES('%s','%s','%s','%s','%s','%s')"%(_firstName, _lastName, _username, _password, _email, str(random.randint(1,100))))
+	    dbhandler.execute("select * from users")
+	    result = dbhandler.fetchall()
+	    for item in result:
+	    	print item
+	    data = {'msg':'New User Registered!','type':'msg'}
+	    return jsonify(data)
+
+    except:
+		data ={'msg': 'Error! Please check that all fields are valid.','type':'error'}
+		print sys.exc_info()[0]
+		return jsonify(data)
+
+    #return render_template('index.html')
